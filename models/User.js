8279d053fdb780d68697527a8f1821e161f42dd9
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-07 15:18:53 +0200

    created user model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const schema = mongoose.Schema();
+
+const UserSchema = new schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  lastName: {
+    type: String,
+    maxlength: 20,
+    trim: true,
+    default: 'lastName',
+  },
+  location: {
+    type: String,
+    maxlength: 20,
+    trim: true,
+    default: 'kafr potata city',
+  },
+});
+
+export default mongoose.model('User', UserSchema);
