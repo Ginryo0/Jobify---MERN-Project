3b09241b20202254812a9b1659fd11902d297990
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-07 15:21:38 +0200

    email validator added to model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const schema = mongoose.Schema();
 
@@ -13,6 +14,10 @@ const UserSchema = new schema({
   email: {
     type: String,
     required: [true, 'Please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email',
+    },
     unique: true,
   },
   password: {
