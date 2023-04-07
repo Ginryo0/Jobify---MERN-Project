002f0c04c78f892e6055725311cf64c8e3c2581e
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-07 14:10:18 +0200

    created job routes

---
+++ controllers/jobControllers.js
@@ -0,0 +1,17 @@
+const createJob = async (req, res) => {
+  res.send('create job');
+};
+const getAllJobs = async (req, res) => {
+  res.send('getAllJobs');
+};
+const updateJob = async (req, res) => {
+  res.send('update job');
+};
+const deleteJob = async (req, res) => {
+  res.send('delete job');
+};
+const showStats = async (req, res) => {
+  res.send('showStats');
+};
+
+export { createJob, deleteJob, getAllJobs, updateJob, showStats };
