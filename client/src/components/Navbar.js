70ced5b10128d6abcc6f253de4f401a31ef7332e
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-09 00:44:36 +0200

    added navbar structure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,37 @@
 import Wrapper from '../assets/wrappers/Navbar';
-import { FaHome } from 'react-icons/fa';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { useAppCtx } from '../context/appContext';
+import Logo from './Logo';
 
 const Navbar = () => {
   return (
     <Wrapper>
-      Navabar <FaHome />
+      <div className="nav-center">
+        <button
+          className="toggle-btn"
+          type="button"
+          onClick={() => console.log('Toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button className="btn" onClick={() => console.log('show logout')}>
+            <FaUserCircle />
+            Meshmesh
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button type="button" className="dropdown-btn">
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
