2032997783f74e9c50bd3e782bc4e0be97039d2b
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-10 06:50:36 +0200

    format date using momentjs

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,4 +1,14 @@
-const Job = ({ company }) => {
-  return <div>{company}</div>;
+import moment from 'moment';
+
+const Job = ({ company, createdAt }) => {
+  let date = moment(createdAt);
+  // format date
+  date = date.format('MMM Do, YYYY');
+  return (
+    <>
+      <div>{company}</div>
+      <div>{date}</div>
+    </>
+  );
 };
 export default Job;
