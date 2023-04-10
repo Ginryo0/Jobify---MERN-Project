507a50b6de2329dc82769e4b653e18426b4d34ef
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-10 16:51:21 +0200

    check permissions before updating

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -6,6 +6,7 @@ import {
 } from '../errors/index.js';
 
 import Job from '../models/Job.js';
+import checkPermissions from '../utils/checkPermissions.js';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -39,17 +40,17 @@ const updateJob = async (req, res) => {
     throw new NotFoundError(`No job was found with id :${jobId}`);
   }
 
-  // we do findOneAndUpdate -> to run validators again I guess
+  checkPermissions(req.user, job.createdBy);
+
+  // we do findOneAndUpdate -> to save our time by updating fields manually & run validators again
   const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
     new: true,
     runValidators: true,
   });
 
-  res.status(StatusCodes.OK).json({ updateJob });
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
-const deleteJob = async (req, res) => {
-  res.send('delete job');
-};
+const deleteJob = async (req, res) => {};
 const showStats = async (req, res) => {
   res.send('showStats');
 };
